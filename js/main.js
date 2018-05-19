08240110b420807b600d2de18d9f1bf7d2b92fd2
Ghada Ali <roneel@windowslive.com>
2018-05-19 20:24:44 +0300

    Accessibility & Offline Availability

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+
+//serviceWorker Registration
+
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker.register('./sw.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
+};
+
+
+
+
 let restaurants,
   neighborhoods,
   cuisines
---
+++ sw.js
@@ -0,0 +1,40 @@
+
+let cachName = "v1";
+let cachFiles = [
+     '/',
+     'css/',
+     'js/',
+     'img/'
+]
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed');
+
+    e.waitUntil(
+
+    	// Open the cache
+	    caches.open(cacheName).then(function(cache) {
+
+	    	// Add all the default files to the cache
+			console.log('[ServiceWorker] Caching cacheFiles');
+			return cache.addAll(cacheFiles);
+	    })
+      .catch(function (data){
+        console.log(data);
+      })
+	);
+
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request)
+    .then(function(response) {
+      if (response) return response;
+      return fetch(e.request);
+    })
+    .catch(function(data) {
+      console.log(data);
+    })
+  )
+});
